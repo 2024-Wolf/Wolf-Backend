bba4528c6709a549ca249ed54da78cff932f65a9
myeongju00 <j51677767@gmail.com>
2024-09-11 14:46:37 +0900

    feat : 불필요한 코드 제거

--- src/main/java/com/kdt/wolf/domain/user/controller/UserController.java
+++ src/main/java/com/kdt/wolf/domain/user/controller/UserController.java
@@ -3,14 +3,11 @@ package com.kdt.wolf.domain.user.controller;
 import com.kdt.wolf.domain.user.dto.response.UserProfileResponse;
 import com.kdt.wolf.domain.user.service.UserService;
 import com.kdt.wolf.global.base.ApiResult;
-import com.kdt.wolf.global.dto.ApiResponse;
-import com.kdt.wolf.global.message.UserMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,12 +15,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
-
-//    @GetMapping("/user/{userId}")
-//    public ApiResponse<UserProfileResponse> getUserProfile(@PathVariable Long userId) {
-//        UserProfileResponse response = userService.getUserProfile(userId);
-//        return ApiResponse.createResponseWithMessage(response, UserMessage.USER_FOUND_SUCCESS);
-//    }
 
     @PostMapping("/testinsertUser")
     public ApiResult<UserProfileResponse> insertUser() {
