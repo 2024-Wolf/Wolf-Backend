b72de26956eb39076832af25b69d245302986224
wing-gahyun <inmeta.kr@gamil.com>
2024-10-01 17:55:25 +0900

    Meeting/GroupMember

---
+++ src/main/java/com/kdt/wolf/domain/group/entity/GroupMember.java
@@ -0,0 +1,48 @@
+package com.kdt.wolf.domain.group.entity;
+
+import com.kdt.wolf.domain.user.entity.UserEntity;
+import com.kdt.wolf.global.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "group_members")
+public class GroupMember extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "group_member_id")
+    private Long groupMemberId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "group_post_id", referencedColumnName = "id", nullable = false)
+    private GroupPost groupPost;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", referencedColumnName = "userId", nullable = false)
+    private UserEntity user;
+
+    @Column(name = "role", length = 50, nullable = false)
+    private String role;
+
+    @Column(name = "position", length = 100, nullable = true)
+    private String position;
+
+    @Builder
+    public GroupMember(GroupPost groupPost, UserEntity user, String role, String position) {
+        this.groupPost = groupPost;
+        this.user = user;
+        this.role = role;
+        this.position = position;
+    }
+
+    public void updateRole(String role) {
+        this.role = role;
+    }
+
+    public void updatePosition(String position) {
+        this.position = position;
+    }
+}
