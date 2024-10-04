4eb73186d9dc660cb4ce695a3bdd83d3f80597a9
myeongju00 <j51677767@gmail.com>
2024-10-04 14:37:22 +0900

    feat : 내정보 보기 API 개발

--- src/main/java/com/kdt/wolf/domain/user/service/UserService.java
+++ src/main/java/com/kdt/wolf/domain/user/service/UserService.java
@@ -1,9 +1,10 @@
 package com.kdt.wolf.domain.user.service;
 
 import com.kdt.wolf.domain.user.dao.UserDao;
-import com.kdt.wolf.domain.user.dto.SignUpDto;
 import com.kdt.wolf.domain.user.dto.SignUpDto.SignUpRequest;
+import com.kdt.wolf.domain.user.dto.UserDto.UserProfileDetailResponse;
 import com.kdt.wolf.domain.user.dto.response.UserProfileResponse;
+import com.kdt.wolf.domain.user.entity.UserEntity;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,11 +13,19 @@ import org.springframework.stereotype.Service;
 public class UserService {
     private final UserDao userDao;
 
+    public UserProfileDetailResponse getUserProfileDetail(Long userId) {
+        UserEntity user = userDao.findById(userId);
+        return user.toUserProfileDetailResponse();
+    }
+
     public UserProfileResponse getUserProfile(Long userId) {
+
         return new UserProfileResponse(userDao.findById(userId));
     }
 
     public void completeSignUpProcess(long userId, SignUpRequest request) {
         userDao.updateUser(userId, request);
     }
+
+
 }
