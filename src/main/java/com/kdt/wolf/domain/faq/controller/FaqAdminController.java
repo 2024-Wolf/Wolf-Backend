6a932ef24f893087ef1ff60a1e840ff4cc6cb449
myeongju00 <j51677767@gmail.com>
2024-10-08 17:09:02 +0900

    feat : FAQ API 스웨거 작성

--- src/main/java/com/kdt/wolf/domain/faq/controller/FaqAdminController.java
+++ src/main/java/com/kdt/wolf/domain/faq/controller/FaqAdminController.java
@@ -5,6 +5,7 @@ import com.kdt.wolf.domain.faq.dto.FaqDto.FaqDetail;
 import com.kdt.wolf.domain.faq.dto.FaqDto.FaqResponse;
 import com.kdt.wolf.domain.faq.dto.FaqDto.FaqUpdateRequest;
 import com.kdt.wolf.domain.faq.service.FaqService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -27,30 +28,35 @@ public class FaqAdminController {
      * **FAQ 수정**: `PATCH /faqs/{faqId}`
      * **FAQ 삭제**: `DELETE /faqs/{faqId}`
      */
+    @Operation(summary = "FAQ 전체 목록 조회")
     @GetMapping("")
     public String getFaqs() {
         FaqResponse response = faqService.getFaqs();
         return "faq";
     }
 
+    @Operation(summary = "FAQ 단일 조회")
     @GetMapping("/{faqId}")
     public String getFaq(@RequestParam Long faqId) {
         FaqDetail faqDetail = faqService.getFaqDetail(faqId);
         return "";
     }
 
+    @Operation(summary = "FAQ 등록")
     @PostMapping("")
     public String createFaq(@RequestBody FaqCreateRequest request) {
         Long id = faqService.createFaq(request);
         return "redirect:/";
     }
 
+    @Operation(summary = "FAQ 수정")
     @PatchMapping("/{faqId}")
     public String updateFaq(@RequestParam Long faqId, @RequestBody FaqUpdateRequest request) {
         FaqDetail faqDetail = faqService.updateFaq(faqId, request);
         return "redirect:/";
     }
 
+    @Operation(summary = "FAQ 삭제")
     @DeleteMapping("/{faqId}")
     public String deleteFaq(@RequestParam Long faqId) {
         Long deleteId = faqService.deleteFaq(faqId);
--- src/main/java/com/kdt/wolf/domain/faq/controller/FaqController.java
+++ src/main/java/com/kdt/wolf/domain/faq/controller/FaqController.java
@@ -3,6 +3,7 @@ package com.kdt.wolf.domain.faq.controller;
 import com.kdt.wolf.domain.faq.dto.FaqDto.FaqResponse;
 import com.kdt.wolf.domain.faq.service.FaqService;
 import com.kdt.wolf.global.base.ApiResult;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class FaqController {
     private final FaqService faqService;
 
+    @Operation(summary = "FAQ 전체 목록 조회(유저)")
     @GetMapping("")
     public ApiResult<FaqResponse> getFaqs() {
         FaqResponse response = faqService.getFaqs();
