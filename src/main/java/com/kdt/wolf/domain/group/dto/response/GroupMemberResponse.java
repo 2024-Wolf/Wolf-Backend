ae3877a77b7a7ef9689176788cd56d816eb2ea44
jnhyk <hjh130101@gmail.com>
2024-10-08 17:23:49 +0900

    refector : GroupMemberResponse 멤버 수정

--- src/main/java/com/kdt/wolf/domain/group/dto/response/GroupMemberResponse.java
+++ src/main/java/com/kdt/wolf/domain/group/dto/response/GroupMemberResponse.java
@@ -1,9 +1,7 @@
 package com.kdt.wolf.domain.group.dto.response;
 
 import com.kdt.wolf.domain.group.entity.GroupMemberEntity;
-import com.kdt.wolf.domain.group.entity.GroupPostEntity;
 import com.kdt.wolf.domain.group.entity.common.MemberRole;
-import com.kdt.wolf.domain.user.entity.UserEntity;
 import lombok.Getter;
 
 @Getter
