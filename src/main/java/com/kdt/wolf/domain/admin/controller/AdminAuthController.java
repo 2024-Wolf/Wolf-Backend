7b72a6f76b68ddd7393791c412604b6853fd5f8c
myeongju00 <j51677767@gmail.com>
2024-10-10 15:59:45 +0900

    feat : admin 토큰 재발급 API 테스트 코드 작성

--- src/main/java/com/kdt/wolf/domain/admin/controller/AdminAuthController.java
+++ src/main/java/com/kdt/wolf/domain/admin/controller/AdminAuthController.java
@@ -3,6 +3,7 @@ package com.kdt.wolf.domain.admin.controller;
 import com.kdt.wolf.domain.admin.service.AdminAuthService;
 import com.kdt.wolf.global.auth.dto.LoginDto.AdminLoginRequest;
 import com.kdt.wolf.global.auth.dto.LoginDto.AdminLogoutRequest;
+import com.kdt.wolf.global.auth.dto.LoginDto.ReissueAccessTokenRequest;
 import com.kdt.wolf.global.auth.dto.LoginDto.TokenResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -25,6 +26,12 @@ public class AdminAuthController {
     @PostMapping("/logout")
     public String logout(@RequestBody AdminLogoutRequest request) {
         adminService.logout(request);
+        return "redirect:/";
+    }
+
+    @PostMapping("/reissue")
+    public String reissueAccessToken(@RequestBody ReissueAccessTokenRequest request) {
+        TokenResponse response = adminService.reissueAccessToken(request.accessToken(), request.refreshToken());
         return "redirect:/";
     }
 }
