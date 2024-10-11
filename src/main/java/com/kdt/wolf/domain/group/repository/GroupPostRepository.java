5e54bf053e0aa8ca18729475be385e76f21a1ef4
jnhyk <hjh130101@gmail.com>
2024-10-11 14:58:21 +0900

    refactor : 멤버명 변경 title -> topic

--- src/main/java/com/kdt/wolf/domain/group/repository/GroupPostRepository.java
+++ src/main/java/com/kdt/wolf/domain/group/repository/GroupPostRepository.java
@@ -11,6 +11,6 @@ import java.util.Optional;
 public interface GroupPostRepository extends JpaRepository<GroupPostEntity, Long> {
     List<GroupPostEntity> findByType(GroupType type);
 
-    @Query("SELECT g FROM GroupPostEntity g WHERE g.title LIKE %:keyword% OR g.description LIKE %:keyword% OR g.tag LIKE %:keyword% OR g.name LIKE %:keyword%")
+    @Query("SELECT g FROM GroupPostEntity g WHERE g.topic LIKE %:keyword% OR g.description LIKE %:keyword% OR g.tag LIKE %:keyword% OR g.name LIKE %:keyword%")
     List<GroupPostEntity> findByKeyword(String keyword);
 }
