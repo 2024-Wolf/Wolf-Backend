d042416c9793b7c04bcbeca06ea8c6028979f76d
myeongju00 <j51677767@gmail.com>
2024-10-18 14:50:18 +0900

    feat : 불필요한 import문 제거

--- src/main/java/com/kdt/wolf/domain/report/controller/ReportAdminController.java
+++ src/main/java/com/kdt/wolf/domain/report/controller/ReportAdminController.java
@@ -1,6 +1,5 @@
 package com.kdt.wolf.domain.report.controller;
 
-import com.kdt.wolf.domain.report.dto.ReportAdminDto.ProcessReportRequest;
 import com.kdt.wolf.domain.report.dto.ReportAdminDto.ReportDetailDto;
 import com.kdt.wolf.domain.report.dto.ReportAdminDto.ReportPreviewDto;
 import com.kdt.wolf.domain.report.service.ReportAction;
@@ -11,10 +10,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
