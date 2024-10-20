d1347ed3010973386ad44ba71c64ed17171ba233
myeongju00 <j51677767@gmail.com>
2024-10-21 05:20:55 +0900

    feat : 알람 Response에 id 추가

--- src/main/java/com/kdt/wolf/domain/alert/dto/AlertDto.java
+++ src/main/java/com/kdt/wolf/domain/alert/dto/AlertDto.java
@@ -7,6 +7,7 @@ import java.time.LocalDateTime;
 public class AlertDto {
 
     public record AlertResponse(
+            Long alertId,
             String alertType,
             String alertContent,
             String alertLink,
