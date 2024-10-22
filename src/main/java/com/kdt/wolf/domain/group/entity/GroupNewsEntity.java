b7dd45ba0b74b0e690614064c1e7b7ac293107ae
myeongju00 <j51677767@gmail.com>
2024-10-23 03:25:52 +0900

    feat : GroupNews 엔티티 생성

---
+++ src/main/java/com/kdt/wolf/domain/group/entity/GroupNewsEntity.java
@@ -0,0 +1,30 @@
+package com.kdt.wolf.domain.group.entity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class GroupNewsEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String newsContent; // 한줄소식 내용
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
+    @JoinColumn(name = "group_post_id", nullable = false)
+    private GroupPostEntity groupPost; // GroupPost와 연관관계
+
+    public GroupNewsEntity(String newsContent, GroupPostEntity groupPost) {
+        this.newsContent = newsContent;
+        this.groupPost = groupPost;
+    }
+
+    public void updateNewsContent(String newContent) {
+        this.newsContent = newContent;
+    }
+}
