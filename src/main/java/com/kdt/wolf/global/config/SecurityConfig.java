1a01b48b2ab4477e37ef3b9bc663ce71aa4e6b4c
myeongju00 <j51677767@gmail.com>
2024-10-23 15:50:24 +0900

    feat : 시큐리티 config에 최식 소식 api 추가

--- src/main/java/com/kdt/wolf/global/config/SecurityConfig.java
+++ src/main/java/com/kdt/wolf/global/config/SecurityConfig.java
@@ -40,7 +40,7 @@ public class SecurityConfig {
                         authz ->
                                 authz.requestMatchers("/api/v1/auth/google", "/api/v1/auth/login", "/api/v1/auth/test-login",
                                                 "/api/v1/auth/reissue", "/api/v1/post/search/{keyword}", "/api/v1/post/{postId}",
-                                                "/api/v1/faqs/{category}", "/api/v1/post/{groupId}/question/{option}").permitAll()
+                                                "/api/v1/faqs/{category}", "/api/v1/post/{groupId}/question/{option}", "/api/v1/post/{groupId}/news").permitAll()
                                         .requestMatchers("/api/v1/**").authenticated()
                                         .requestMatchers(HttpMethod.GET, "/api/v1/post/{options}").permitAll()
                                         .requestMatchers(HttpMethod.POST, "/admin/auth/login").permitAll()
