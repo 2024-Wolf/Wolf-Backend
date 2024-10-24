79e2d1c611bf2ff86212d0cc401fc73c66c4a114
jnhyk <hjh130101@gmail.com>
2024-10-24 14:01:54 +0900

    fix : 이미 있는 type에 대해 수정이 아닌 create을 한번 더 할 경우 예외처리

--- src/main/java/com/kdt/wolf/domain/link/repository/ExternalLinksRepository.java
+++ src/main/java/com/kdt/wolf/domain/link/repository/ExternalLinksRepository.java
@@ -1,16 +1,18 @@
 package com.kdt.wolf.domain.link.repository;
 
+import com.kdt.wolf.domain.group.entity.common.LinkType;
 import com.kdt.wolf.domain.link.entity.ExternalLinksEntity;
 import com.kdt.wolf.domain.group.entity.GroupPostEntity;
 import com.kdt.wolf.domain.user.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import org.springframework.data.jpa.repository.Query;
 
 public interface ExternalLinksRepository extends JpaRepository<ExternalLinksEntity, Long> {
     List<ExternalLinksEntity> findALLByGroupPost(GroupPostEntity groupPost);
 
 //    @Query("SELECT e FROM ExternalLinksEntity e WHERE e.user.userId = :user")
     List<ExternalLinksEntity> findALLByUser(UserEntity user);
+
+    boolean existsByGroupPostAndLinkType(GroupPostEntity groupPost, LinkType linkType);
 }
